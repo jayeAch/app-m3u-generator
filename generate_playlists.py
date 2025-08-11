d335fbff5f139714e906cb6202480a78b48bd46d
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-08-10 23:45:38 -0400

    Update generate_playlists.py

--- generate_playlists.py
+++ generate_playlists.py
@@ -472,7 +472,7 @@ if __name__ == "__main__":
     ]
     
     # Default regions for services that support them
-    regions = ['us', 'ca', 'gb', 'au', 'all']
+    regions = ['nl', 'ie', 'th', 'be', 'lu', 'pt', 'fi', 'se', 'th', 'sg', 'ph', 'ar', 'br', 'ca', 'cl', 'co', 'cr', 'de', 'dk', 'do','ec', 'es', 'fr', 'gb', 'gt', 'it', 'mx' , 'no', 'pe', 'se', 'us', 'latam', 'all']
     
     # Generate playlists for each service
     for service in services:
